2d14e2d7fb09447474593e9d21757d1d8173caf7
NameIsUser06 <woosoungcho@gmail.com>
2023-12-28 12:05:00 +0900

    fix : logging

--- src/main/java/org/bssm/attachit/domain/attachment/service/FileSaveUtil.java
+++ src/main/java/org/bssm/attachit/domain/attachment/service/FileSaveUtil.java
@@ -18,14 +18,26 @@ public class FileSaveUtil {
 
     public String save(MultipartFile file) {
         try {
+            System.out.println(file);
+            // 클라이언트에서 전송한 파일 이름 얻기
+            System.out.println(file.getOriginalFilename());
+
+            // 파일 이름 생성 (UUID와 클라이언트에서 전송한 파일 이름 조합)
             String fileName = UUID.randomUUID().toString().replace("-", "") + "_" + file.getOriginalFilename();
-            System.out.println(fileProperties.getPath());
-            Path path = fileProperties.getPath().resolve(
-                    Paths.get(fileName)
-                            .normalize());
+
+            System.out.println(fileName);
+
+            // 파일 경로 설정
+            Path path = fileProperties.getPath().resolve(fileName);
+
             System.out.println(path);
-            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("ok");
+
+            // 파일 저장
+            Files.write(path, file.getBytes(), StandardOpenOption.CREATE);
+
+            System.out.println("clear");
+
+            // 저장된 파일 경로 반환
             return String.format("%s/%s", fileProperties.getPath(), fileName);
         } catch (IOException e) {
             throw FileException.EXCEPTION;
